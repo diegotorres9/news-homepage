450be8935c11dccb2905c163852393b7d1e97587
Diego Torres <diegoetorres1@gmail.com>
2024-11-25 21:17:20 -0500

    toggle overlay on menu open and close

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 const menuIcon = document.getElementById("menu-icon");
 const closeIcon = document.getElementById("menu-close-icon");
 const mobileNav = document.getElementById("mobile-nav");
+const overlay = document.getElementById("overlay");
 
 menuIcon.addEventListener("click", (e) => {
     // mobileNav.style.display = "block";
     mobileNav.classList.toggle("is-open");
-    closeIcon.classList.toggle("is-open")
+    closeIcon.classList.toggle("is-open");
+    overlay.classList.toggle("is-open");
 })
 closeIcon.addEventListener("click", (e) => {
     // mobileNav.style.display = "none";
     mobileNav.classList.toggle("is-open");
     closeIcon.classList.toggle("is-open");
+    overlay.classList.toggle("is-open");
 })
